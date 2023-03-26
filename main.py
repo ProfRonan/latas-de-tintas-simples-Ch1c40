606fb0639fa893ba7fb6873307cad2397eabb2a8
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-03-26 18:15:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ if result > 18:
     qtd_de_latas = int(result / 3) + 1
     valor_total = qtd_de_latas * 80
     print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
-elif result <= 18 and result > 0:
+if result <= 18 and result > 0:
     qtd_de_latas = 1
     valor_total = qtd_de_latas * 80
     print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
-if result == 0:
+if result = 0:
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-qtd_de_latas = 0
-valor_total = 0
+qtd_de_latas = 1
+valor_total = 80
 
 print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
