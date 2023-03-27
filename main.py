a78fd11b615c841a555af1a48732b60723b38cc5
Francisco Arley Coelho Barros <107206805+Ch1c40@users.noreply.github.com>
2023-03-27 10:48:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,15 @@ metros_quadrados = input("Qual a área em m²?\n")
 metros_quadrados = float(metros_quadrados)
 
 # Coloque o código para resolver o problema nessa parte do programa
-result = metros_quadrados / 3
+import math
+litros = metros_quadrados / 3
+latas = litros / 18
 
-if result > 18:
-    qtd_de_latas = int(result / 3) + 1
-    valor_total = qtd_de_latas * 80
-    print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
-if result <= 18 and result > 0:
-    qtd_de_latas = 1
-    valor_total = qtd_de_latas * 80
-    print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
-if result = 0:
-    
+qtd_de_latas = math.ceil(latas)
+valor_total = qtd_de_latas*80
 # As duas variáveis qtd_de_latas e valor_total devem guardar a resposta do problema
 # Troque os zeros pelos valores corretos.
-    qtd_de_latas = 1
-    valor_total = 80
+qtd_de_latas = qtd_de_latas
+valor_total = valor_total
 
 print(f"Serão necessárias {qtd_de_latas} latas totalizando R$ {valor_total}")
